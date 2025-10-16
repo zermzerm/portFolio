6c3ad7e144d967926f4ce13d72fbae06c80559e3
zermzerm <zerm0907@gmail.com>
2025-10-16 15:53:24 +0900

    nav바 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,59 @@
+"use client";
+
 import "./globals.css";
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
+  const handleScroll = (id: string) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({behavior: "smooth"});
+    }
+  };
+
   return (
     <html lang="ko">
       <body>
         <nav className="text-white absolute w-full h-[50px] text-[50px] font-bold flex justify-evenly items-center">
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-          <div>4</div>
+          <div
+            onClick={() => {
+              handleScroll("main");
+            }}
+            className="cursor-pointer"
+          >
+            main
+          </div>
+          <div
+            onClick={() => {
+              handleScroll("about");
+            }}
+            className="cursor-pointer"
+          >
+            about
+          </div>
+          <div
+            onClick={() => {
+              handleScroll("skills");
+            }}
+            className="cursor-pointer"
+          >
+            skills
+          </div>
+          <div
+            onClick={() => {
+              handleScroll("projects");
+            }}
+            className="cursor-pointer"
+          >
+            projects
+          </div>
+          <div
+            onClick={() => {
+              handleScroll("contact");
+            }}
+            className="cursor-pointer"
+          >
+            contact
+          </div>
         </nav>
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 export default function Home() {
   return (
     <main className="overflow-auto h-full snap-y snap-mandatory scroll-smooth">
-      <section className="min-h-screen snap-start bg-[#e3d7ea]" id="home">
+      <section className="min-h-screen snap-start bg-[#e3d7ea]" id="main">
         main
       </section>
       <section className="min-h-screen snap-start bg-[#d7e3ea]" id="about">
         about
       </section>
       <section className="min-h-screen snap-start bg-[#e3e3ea]" id="skills">
-        skill
+        skills
       </section>
       <section className="min-h-screen snap-start bg-[#e3ead7]" id="projects">
         projects
