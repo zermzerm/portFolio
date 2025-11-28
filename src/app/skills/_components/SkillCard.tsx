1bdb18c563c7afeec8a758371614129b3b161fc0
zermzerm <zerm0907@gmail.com>
2025-11-28 17:27:09 +0900

    feat : 색칠놀이

--- src/app/skills/_components/SkillCard.tsx
+++ src/app/skills/_components/SkillCard.tsx
@@ -10,7 +10,7 @@ interface SkillType {
 
 export default function SkillCard({type}: SkillType) {
   return (
-    <div className={`flex flex-col gap-2 ${type.variant && "p-5 bg-[#7e81cb] rounded-2xl"}`}>
+    <div className={`flex flex-col gap-2 ${type.variant && "p-5 bg-[#bfb9e6] rounded-2xl"}`}>
       <h3 className={`${type.variant && "text-2xl text-center pb-5"} text[16px]`}>{type.name}</h3>
       <ul className="flex gap-3">
         {type.images.map((el, idx) => (
--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -4,12 +4,12 @@ import {SKILLS} from "@/constants/skill";
 export default function Skills() {
   return (
     <section
-      className="py-[96px] bg-[#d7e3ea] flex flex-col justify-center items-center"
+      className="py-[96px] bg-[#F8F0FC] flex flex-col justify-center items-center"
       id="skills"
     >
-      <h1 className="text-[32px] font-bold">Skills</h1>
-      <div className="flex gap-5 p-5 text-white">
-        <div className="p-5 bg-[#7e81cb] rounded-2xl flex flex-col gap-5">
+      <h1 className="text-[32px] font-bold text-[#2F4F4F]">Skills</h1>
+      <div className="flex gap-5 p-5 text-[#2F4F4F]">
+        <div className="p-5 bg-[#bfb9e6] rounded-2xl flex flex-col gap-5">
           <h2 className="text-2xl text-center">Technology Stacks</h2>
           {/* 언어 및 프레임워크 */}
           <SkillCard type={SKILLS.languages} />
