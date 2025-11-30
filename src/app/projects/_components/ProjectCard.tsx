2194f591ca2f6a2ac10aeb496aa3d78c7e7cfea2
zermzerm <zerm0907@gmail.com>
2025-11-30 16:19:26 +0900

    feat : project 틀 완성

--- src/app/projects/_components/ProjectCard.tsx
+++ src/app/projects/_components/ProjectCard.tsx
@@ -23,11 +23,11 @@ interface ProjectProps {
 
 export default function ProjectCard({data}: ProjectProps) {
   return (
-    <div className="flex flex-col gap-5 p-5 bg-[#9085d2] text-white rounded-2xl">
+    <div className="flex flex-col gap-5 p-5 w-[1100px] bg-[#9085d2] text-white rounded-2xl">
       <h2 className="text-center text-2xl">{data.name}</h2>
       <div className="text-center">{data.date}</div>
-      <div className="flex justify-around gap-5">
-        <div className="flex flex-col gap-3 justify-center items-center">
+      <div className="flex justify-between gap-5">
+        <div className="flex-1 flex flex-col gap-3 justify-center items-center">
           <div className="w-[400px] h-[400px] overflow-hidden rounded-xl">
             <Swiper
               pagination={{
@@ -55,20 +55,24 @@ export default function ProjectCard({data}: ProjectProps) {
               ))}
             </Swiper>
           </div>
-
-          <div>기술스택</div>
-          <div>{data.skills}</div>
           <div>{data.github}</div>
           <div>{data.deployment}</div>
         </div>
-        <div className="flex flex-col gap-5 justify-between">
+
+        <div className="flex flex-1 flex-col gap-5 ">
           <div>
-            <h3 className="text-[20px]">{data.name}</h3>
+            <h3 className="text-[20px] font-bold">{data.name}</h3>
             <div>{data.description}</div>
           </div>
           <div>
-            <div>역할 및 성과</div>
-            <div>{data.accomplishment[0]}</div>
+            <h3 className="text-[20px] font-bold">기술스택</h3>
+            <div>{data.skills}</div>
+          </div>
+          <div>
+            <h3 className="text-[20px] font-bold">역할 및 성과</h3>
+            {data.accomplishment.map((el, idx) => (
+              <div key={idx}>{el}</div>
+            ))}
           </div>
         </div>
       </div>
