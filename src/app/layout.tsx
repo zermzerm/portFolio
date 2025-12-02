32a4435626a9f2fac534e347ad58bcc661e58bc1
zermzerm <zerm0907@gmail.com>
2025-12-02 16:38:30 +0900

    fix : meta 태그 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,13 @@
 import Navbar from "@/components/Navbar";
 import "./globals.css";
-import {Metadata} from "next";
 
-export const metadata: Metadata = {
-  title: "PKS Portfolio",
-  description: "내가 만든 포트폴리오 사이트",
-  openGraph: {
-    title: "PKS Portfolio",
-    description: "내가 만든 포트폴리오 사이트",
-    images: ["/face.png"],
-  },
-};
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="ko">
       <head>
+        <meta property="og:title" content="PKS Portfolio" />
+        <meta property="og:description" content="내가 만든 포트폴리오 사이트" />
+        <meta property="og:image" content="https://postimg.cc/QBvYtnSX" />
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.css"
