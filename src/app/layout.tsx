b905d4716fdb4ea0e02d68eb2a9037afb8bdf4bf
zermzerm <zerm0907@gmail.com>
2025-12-02 16:44:59 +0900

    fix : meta 태그 변경2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+// app/layout.tsx
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 
@@ -5,9 +6,16 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="ko">
       <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>PKS Portfolio</title>
+        <meta name="description" content="내가 만든 포트폴리오 사이트" />
+        <meta property="og:type" content="website" />
         <meta property="og:title" content="PKS Portfolio" />
         <meta property="og:description" content="내가 만든 포트폴리오 사이트" />
-        <meta property="og:image" content="https://postimg.cc/QBvYtnSX" />
+        <meta property="og:image" content="https://pks-portfolio-v2.vercel.app/face.png" />
+        <meta property="og:url" content="https://pks-portfolio-v2.vercel.app" />
+
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.css"
