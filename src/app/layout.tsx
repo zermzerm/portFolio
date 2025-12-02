edbd468da18f58fd1c45b89f522cb28aeb78a3d1
zermzerm <zerm0907@gmail.com>
2025-12-02 16:30:52 +0900

    feat : meta태그 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,16 @@
 import Navbar from "@/components/Navbar";
 import "./globals.css";
+import {Metadata} from "next";
 
+export const metadata: Metadata = {
+  title: "PKS Portfolio",
+  description: "내가 만든 포트폴리오 사이트",
+  openGraph: {
+    title: "PKS Portfolio",
+    description: "내가 만든 포트폴리오 사이트",
+    images: ["/face.png"],
+  },
+};
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="ko">
