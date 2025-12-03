d51d51361875322e58f22b6662be80800cb4dcbd
zermzerm <zerm0907@gmail.com>
2025-12-03 13:15:50 +0900

    fix : favicon 변경, metadata 방식 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,26 @@
 import Navbar from "@/components/Navbar";
 import "./globals.css";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+  title: "PKS Portfolio",
+  description: "내가 만든 포트폴리오 사이트",
+  icons: {
+    icon: "/favicon.png",
+  },
+  openGraph: {
+    type: "website",
+    url: "https://pks-portfolio-v2.vercel.app",
+    title: "PKS Portfolio",
+    description: "내가 만든 포트폴리오 사이트",
+    images: ["https://pks-portfolio-v2.vercel.app/face.png"],
+  },
+};
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="ko">
       <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>PKS Portfolio</title>
-        <meta name="description" content="내가 만든 포트폴리오 사이트" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="PKS Portfolio" />
-        <meta property="og:description" content="내가 만든 포트폴리오 사이트" />
-        <meta property="og:image" content="https://pks-portfolio-v2.vercel.app/face.png" />
-        <meta property="og:url" content="https://pks-portfolio-v2.vercel.app" />
-
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.css"
